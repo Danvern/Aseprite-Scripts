9a3cde44d0aab6a837b9b871c17f1c3296b85884
Artificer 111 <artificeroneeleven@gmail.com>
2021-10-20 11:55:37 -0400

    Clean up legacy code and implemented sittings window to be easily decoupled from plug in

--- [A111] AA HelperTestScript.lua
+++ [A111] AA HelperTestScript.lua
@@ -4,19 +4,81 @@ if not spr then return end
 local baseSelection = spr.selection
 if baseSelection.isEmpty then return end
 
-local aMax=50
-local aMin=0
-local aScale=1.0
-local aInside=true
-local aAutomate=true
-local aTransparency=true
-local aConcaveSpacing=2
-local aConcaveScale=1.0
-local aAverageInsideColor=true
+aMax=50
+aMin=0
+aScale=1.0
+aInside=true
+aAutomate=true
+aTransparency=true
+aConcaveSpacing=2
+aConcaveScale=1.0
+aAverageInsideColor=true
 -- "constant", "linear", "corner bias"
-local aAverageInsideColorFormula="normal bias"
+aAverageInsideColorFormula="normal bias"
 
-function run()
+function cutCornersDialogue()
+    local spr = app.activeSprite
+    if not spr then return end
+
+    local baseSelection = spr.selection
+    if baseSelection.isEmpty then return end
+
+    local info = Dialog()
+        info:label{ 
+            id=string,
+            label="-------- AA Assist Control Panel --------",
+            text="Set percentages and other values to control the selection area."
+        }
+        info:slider{
+            id="aliasMax",
+            label="Max Threshold",
+            min=0,
+            max=100,
+            value=aMax
+        }
+        info:slider{
+            id="aliasMin",
+            label="Min Threshold",
+            min=0,
+            max=100,
+            value=aMin
+        }
+        info:check{
+            id="aliasInside", 
+            label="AA Inside Selection", 
+            text="Anti-alias inside of the selection versus outside of it.", 
+            selected=aInside
+        }
+        info:check{
+            id="aliasAutomatic",
+            label="AA Automatically", 
+            text="Automatically apply colors instead of stenciling the selection.", 
+            selected=aAutomate
+        }
+        info:button{
+            id="resetSettings",
+            text="Reset Settings", 
+            onclick=function()
+                info.data.aliasMax=50
+                info.data.aliasMin=0
+                info.data.aliasInside=false
+                info.data.aliasAutomatic=false
+                print("(WIP) Settings Have Been Reset")
+            end
+        }
+        info:button{id="cancel",text="Cancel"}
+        info:button{id="ok",text="OK"}
+        info:show()
+        
+    aMax=info.data.aliasMax
+    aMin=info.data.aliasMin
+    aScale=1.0
+    aInside=info.data.aliasInside
+    aAutomate=info.data.aliasAutomatic
+    return info.data.ok
+end
+        
+function cutCorners()
     function getAdjacent(x, y)
         adj={}
         adj[1]={x-1, y}
@@ -34,7 +96,7 @@ function run()
         result = 0
         -- print(adj)
         -- print(string.format("%d, %d", x, y))
-        adj = getAdjacent(x, y)
+        local adj = getAdjacent(x, y)
         for index, coord in ipairs(adj) do
             ax = coord[1]
             ay = coord[2]
@@ -60,7 +122,6 @@ function run()
     -- iterate through the boundaries of selection to add corner pixels to a table
     local rectangle = baseSelection.bounds
     local corners = {}
-    -- print(rectangle) 
     for x = rectangle.x, rectangle.width + rectangle.x, 1 do
         for y = rectangle.y, rectangle.height + rectangle.y, 1 do
             -- print("test0")
@@ -89,7 +150,7 @@ function run()
         
         -- Rotate facing by specified amount of offsets. Positive is clockwise.
         function rotateFacing(facing, spin)
-            face = facing - 1 + spin
+            local face = facing - 1 + spin
             if face < 0 then
                 face = face % -8 + 8
             else
@@ -99,15 +160,15 @@ function run()
         end
         
         function checkDirection(direction)
-            checkX = directionsX[direction] + driver[1]
-            checkY = directionsY[direction] + driver[2]
+            local checkX = directionsX[direction] + driver[1]
+            local checkY = directionsY[direction] + driver[2]
             return {["x"] = checkX, ["y"] = checkY}
         end
         
         -- 
         function checkFacingEdge(direction)
-            point = checkDirection(direction)
-            checkAdjacency = adjacencyCount(point.x, point.y)
+            local point = checkDirection(direction)
+            local checkAdjacency = adjacencyCount(point.x, point.y)
             -- print(string.format("(%d, %d) adjacent %d", checkX, checkY, checkAdjacency))
             return baseSelection:contains(point.x, point.y) and checkAdjacency < 4
         end
@@ -115,10 +176,10 @@ function run()
         -- Check if facing is navigable then return direction of nearby border. Favors counter clockwise movement due to top left corner ordering.
         function checkHugDirection(direction)
             -- print("initiate hug direction check")
-            clockX = directionsX[rotateFacing(direction, 1)] + driver[1]
-            clockY = directionsY[rotateFacing(direction, 1)] + driver[2]
-            counterX = directionsX[rotateFacing(direction, -1)] + driver[1]
-            counterY = directionsY[rotateFacing(direction, -1)] + driver[2]
+            local clockX = directionsX[rotateFacing(direction, 1)] + driver[1]
+            local clockY = directionsY[rotateFacing(direction, 1)] + driver[2]
+            local counterX = directionsX[rotateFacing(direction, -1)] + driver[1]
+            local counterY = directionsY[rotateFacing(direction, -1)] + driver[2]
             -- print(string.format("Clock: %d, %d - Counter: %d, %d", clockX, clockY, counterX, counterY))
             -- print("checking direction...")
             if checkFacingEdge(direction) then
@@ -147,11 +208,11 @@ function run()
         -- Perform calculations if not already exploited.
         if exploitedPixels[driver[1] * rectangle.height + driver[2]] == nil then
             -- print("started border web at: "..table.concat(driver, ", "))
-            iteration = 0
-            timeout = 0
+            local iteration = 0
+            local timeout = 0
             -- to ensure a clean starting strand
-            cleanOrigin = {}
-            spinDirection = checkHugDirection(facing)
+            local cleanOrigin = {}
+            local spinDirection = checkHugDirection(facing)
             while(spinDirection == 0 and timeout < 8) do
                 -- print(string.format("facing: %d", facing))
                 facing = rotateFacing(facing, 1)
@@ -168,10 +229,6 @@ function run()
                     end
                     -- Check if facing is navigable without direction change, advance and add the coordinate to strand.
                     strand = {}
-                    -- while(checkFacingEdge(facing) and not checkFacingEdge(rotateFacing(facing, spinDirection * -1))
-                    -- and not (checkFacingEdge(rotateFacing(facing, spinDirection * -2))
-                    -- and baseSelection:contains(checkDirection(rotateFacing(facing, spinDirection * -1)).x,
-                    -- checkDirection(rotateFacing(facing, spinDirection * -1)).y))) do
                     while(checkFacingEdge(facing) and not checkFacingEdge(rotateFacing(facing, spinDirection * -1))
                     and not (checkFacingEdge(rotateFacing(facing, spinDirection * -2))
                     and (baseSelection:contains(checkDirection(rotateFacing(facing, spinDirection * -1)).x,
@@ -209,9 +266,9 @@ function run()
     
     -- general purpose calculation
     function calculatePixel(point, strand, index, primaryVertexOffset, scale)
-        pixel = {}
-        cornerIndex = 1
-        normalOffset = 2
+        local pixel = {}
+        local cornerIndex = 1
+        local normalOffset = 2
         if primaryVertexOffset > 0 then
             cornerIndex = 1
             normalOffset = -2
@@ -232,7 +289,7 @@ function run()
         pixel.sourceY = strand.components[cornerIndex].y
         pixel.compareX = strand.components[cornerIndex].x + directionsX[rotateFacing(strand.normalFacing, normalOffset)]
         pixel.compareY = strand.components[cornerIndex].y + directionsY[rotateFacing(strand.normalFacing, normalOffset)]
-        percent = 0.0
+        local percent = 0.0
         if aInside then
             if primaryVertexOffset > 0 then
             -- print(index)
@@ -245,9 +302,9 @@ function run()
             if primaryVertexOffset > 0 then
             -- print(index)
             -- print(#strand.components)
-                percent = clamp(1.0, (index - 1) / (#strand.components * aScale), 0.0)
+                percent = clamp(1.0, (index - 1) / (#strand.components * aScale * scale), 0.0)
             else
-                percent = 1.0 - clamp(1.0, index / (#strand.components * aScale), 0.0)
+                percent = clamp(1.0, (1.0 - index / #strand.components) / (aScale * scale), 0.0)
             end
         end
         pixel.percent = percent
@@ -258,7 +315,7 @@ function run()
     end
     
     function strandSize(strandIndex, offset, web)
-        comparisonIndex = strandIndex - 1 + offset
+        local comparisonIndex = strandIndex - 1 + offset
         if comparisonIndex < 0 then
             comparisonIndex = comparisonIndex % -#web + #web
         else
@@ -273,7 +330,7 @@ function run()
     end
     
     function facingChange(strandIndex, offset, web)
-        comparisonIndex = strandIndex - 1 + offset
+        local comparisonIndex = strandIndex - 1 + offset
         if comparisonIndex < 0 then
             comparisonIndex = comparisonIndex % -#web + #web
         else
@@ -283,9 +340,9 @@ function run()
         if comparisonIndex > #web then
             return 0
         end
-        difference = 0
-        clockDifference = web[comparisonIndex].normalFacing - web[strandIndex].normalFacing
-        counterDifference = math.max(clockDifference - 8, -clockDifference - 8)
+        local difference = 0
+        local clockDifference = web[comparisonIndex].normalFacing - web[strandIndex].normalFacing
+        local counterDifference = math.max(clockDifference - 8, -clockDifference - 8)
         if clockDifference < 0 then
             counterDifference = counterDifference * -1
         end
@@ -435,76 +492,7 @@ function run()
             print("other generation complete")
         end
     end
-    
-    -- expand outwards from corner pixel to define a partial outline based on thresholds
-    edgeCrawl=function(x, y, borderWeb)
-        for index, coord in ipairs(getAdjacent(x, y)) do
-            ax = coord[1]
-            ay = coord[2]
-            strand = {}
-            
-            d = 0
-            -- strand cell core
-            cx = (ax - x)
-            cy = (ay - y)
-            -- adjacent to current strand cell
-            sx = math.abs(ay - y)
-            sy = math.abs(ax - x)
-            
-            if not baseSelection:contains(ax, ay) and not aInside then
-                -- if the selected region is in a positive direction relative to the border
-                positive = baseSelection:contains(ax + cx * d + sx, ay + cy * d + sy)
-                while ((positive and baseSelection:contains(ax + cx * d + sx, ay + cy * d + sy)) or
-                (not positive and baseSelection:contains(ax + cx * d - sx, ay + cy * d - sy)))
-                and not baseSelection:contains(ax + cx * d, ay + cy * d) do
-                    pixel = {ax + cx * d, ay + cy * d, x, y, sx, sy, 0}
-                    table.insert(strand, pixel)
-                    --print(string.format("Tested: %d, %d, %d, %d, %d, %d", pixel[1], pixel[2], pixel[3], pixel[4], pixel[5], pixel[6]))
-                    d = d + 1
-                end
-            elseif aInside then
-                -- inside corner is part of selection
-                table.insert(strand, 1, {x, y, x, y, sx, sy, 1})
-                -- if the selected region is in a positive direction relative to the border
-                positive = not baseSelection:contains(ax + cx * d - sx, ay + cy * d - sy)
-                while ((positive and not baseSelection:contains(ax + cx * d - sx, ay + cy * d - sy)) or
-                (not positive and not baseSelection:contains(ax + cx * d + sx, ay + cy * d + sy)))
-                and baseSelection:contains(ax + cx * d, ay + cy * d) do
-                    -- the coordinates of the target pixel, origin pixel, and sx/sy
-                    pixel = {ax + cx * d, ay + cy * d, x, y, sx, sy, 1}
-                    table.insert(strand, pixel)
-                    --print(string.format("Tested: %d, %d, %d, %d, %d, %d", pixel[1], pixel[2], pixel[3], pixel[4], pixel[5], pixel[6]))
-                    d = d + 1
-                end
-            end
-                
-            if #strand > 0 then
-                for i=math.floor(#strand*(aMin/100)), math.floor(#strand*(aMax/100)), 1 do
-                    if strand[i] ~= nil then
-                 -- print(#strand)
-                    -- table.insert(borderWeb, {strand[i], i / #strand})
-                    --  print(strand[i])
-                    -- print(string.format("Tested: %d, %d for total %d", strand[i], strand[i][1], strand[i][2]))
-                    --print("wooo")
-                        -- TO DO: add custom scaling threshold
-                        if aInside then
-                            strand[i][7] = clamp(1.0, i / (#strand * aScale), 0.0)
-                        else
-                            strand[i][7] = 1.0 - clamp(1.0, i / ((#strand) * aScale), 0.0)
-                        end
-                        --table.insert(borderWeb, strand[i])
-                    end
-                end
-                table.insert(borderWeb, {strand, cx, cy})
-            end
-        end
-    end
-    
-    -- for index, coord in ipairs(corners) do
-        -- edgeCrawl(coord[1], coord[2], borderWeb)
-    -- end
-                
-    
+
     -- color selection
     if aAutomate and #aliasPixels > 0 then
         spr.selection = Selection()
@@ -520,7 +508,7 @@ function run()
                     elseif source == c2 then c2 = c1
                     end
                 end
-                realPercent = percent
+                local realPercent = percent
                 if not aTransparency then
                     if pc.rgbaA(c1) == 0 then                    
                         realPercent = 0.0
@@ -532,10 +520,10 @@ function run()
             end
             
             function mixColour(c1, c2, mask, percent)
-                rVal = mixClean(c1, c2, mask, pc.rgbaR, percent)
-                gVal = mixClean(c1, c2, mask, pc.rgbaG, percent)
-                bVal = mixClean(c1, c2, mask, pc.rgbaB, percent)
-                aVal = 255
+                local rVal = mixClean(c1, c2, mask, pc.rgbaR, percent)
+                local gVal = mixClean(c1, c2, mask, pc.rgbaG, percent)
+                local bVal = mixClean(c1, c2, mask, pc.rgbaB, percent)
+                local aVal = 255
                 if aTransparency then
                     aVal = mixClean(c1, c2, mask, pc.rgbaA, percent)
                 end
@@ -543,11 +531,11 @@ function run()
             end
 
             if aInside and pixel.percent < 1 then
-                sourceValue = sourceImage:getPixel(pixel.x - cel.position.x, pixel.y - cel.position.y)
-                inletValue = sourceImage:getPixel(pixel.compareX - cel.position.x, pixel.compareY - cel.position.y)
+                local sourceValue = sourceImage:getPixel(pixel.x - cel.position.x, pixel.y - cel.position.y)
+                local inletValue = sourceImage:getPixel(pixel.compareX - cel.position.x, pixel.compareY - cel.position.y)
                 if aAverageInsideColor then
-                    normalValue = sourceImage:getPixel(pixel.normalX - cel.position.x, pixel.normalY - cel.position.y)
-                    cornerValue = sourceImage:getPixel(pixel.sourceX - cel.position.x, pixel.sourceY - cel.position.y)
+                    local normalValue = sourceImage:getPixel(pixel.normalX - cel.position.x, pixel.normalY - cel.position.y)
+                    local cornerValue = sourceImage:getPixel(pixel.sourceX - cel.position.x, pixel.sourceY - cel.position.y)
                     if aAverageInsideColorFormula == "linear" then
                         inletValue = mixColour(normalValue, inletValue, nil, pixel.percent)
                     elseif aAverageInsideColorFormula == "normal bias" then
@@ -564,8 +552,8 @@ function run()
                 -- print(string.format("S:(%d, %d), C:(%d, %d), %f P", pixel.x, pixel.y, pixel.compareX, pixel.compareY, pixel.percent))
                 image:drawPixel(pixel.x - cel.position.x, pixel.y - cel.position.y, mixColour(sourceValue, inletValue, nil, pixel.percent))
             elseif not aInside and pixel.percent > 0 then
-                sourceValue = sourceImage:getPixel(pixel.sourceX - cel.position.x, pixel.sourceY - cel.position.y)
-                underValue = sourceImage:getPixel(pixel.x - cel.position.x, pixel.y - cel.position.y)
+                local sourceValue = sourceImage:getPixel(pixel.sourceX - cel.position.x, pixel.sourceY - cel.position.y)
+                local underValue = sourceImage:getPixel(pixel.x - cel.position.x, pixel.y - cel.position.y)
                 -- print(string.format("U:(%d, %d), S:(%d, %d), %f P", pixel.x, pixel.y, pixel.sourceX, pixel.sourceY, pixel.percent))
                 image:drawPixel(pixel.x - cel.position.x, pixel.y - cel.position.y, mixColour(sourceValue, underValue, nil, pixel.percent))
             end
@@ -574,7 +562,7 @@ function run()
         app.activeImage:drawImage(image)
     elseif #aliasPixels > 0 then
          -- returned found pixels as a selection
-        newSelection = Selection()
+        local newSelection = Selection()
         for index, pixel in ipairs(aliasPixels) do
             if (pixel.percent > 0 and not aInside) or (pixel.percent < 1 and aInside) then
                 newSelection:add(Selection(Rectangle(pixel.x, pixel.y, 1, 1)))
@@ -586,9 +574,8 @@ function run()
     end
 end
 
--- if info.data.ok then
-if true then
-    app.transaction(run)
+if cutCornersDialogue() then
+    app.transaction(cutCorners())
 end
 
 app.refresh()
