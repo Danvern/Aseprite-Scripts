57578fb2b0ce1a0a58aee62ee5dbfa0acdbb6d0b
Artificer 111 <artificeroneeleven@gmail.com>
2025-02-08 23:09:38 -0500

    Setting up requires for tests.

--- spec/aatool/test.lua
+++ spec/aatool/test.lua
@@ -4,8 +4,8 @@ function testPass()
 	luaunit.assertEquals({ 1, 2, 3 }, { 1, 2, 3 })
 end
 
-function testFail()
-	luaunit.assertEquals({ 1, 2, 3 }, { 1, 2, 6 })
-end
+-- function testFail()
+-- 	luaunit.assertEquals({ 1, 2, 3 }, { 1, 2, 6 })
+-- end
 
 os.exit(luaunit.LuaUnit.run())
