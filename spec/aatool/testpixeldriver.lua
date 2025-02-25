41d11f2709dc629a1035d971e886a581ee3caae8
Artificer 111 <artificeroneeleven@gmail.com>
2025-02-24 21:54:24 -0500

    Updated Initiation text to correct coordinate labels.

--- spec/aatool/testpixeldriver.lua
+++ spec/aatool/testpixeldriver.lua
@@ -13,7 +13,7 @@ function testInitiation()
 
 	luaunit.assertEquals(newDriver.facing, 1, "Facing not initialized to 1")
 	luaunit.assertEquals(newDriver.spinDirection, 0, "Facing not initialized to 0 (no inherent spin)")
-	luaunit.assertEquals(newDriver.driverXY, { 0, 0 }, "Position not initialized to { 0, 0 }")
+	luaunit.assertEquals(newDriver.driverXY, { x=0, y=0 }, "Position not initialized to { 0, 0 }")
 	luaunit.assertEquals(newDriver.borderWeb, { }, "Border web not initialized to empty list")
 	luaunit.assertEquals(newDriver.webCluster, { }, "Web cluster not initialized to empty list")
 	luaunit.assertEquals(newDriver.visitedPixels, { }, "Visited pixels not initialized to empty list")
